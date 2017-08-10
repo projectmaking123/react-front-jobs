bc70e3deabfb46b58ff4b9ea2cc28e46832be5e6
projectmaking123 <jason.89.wang@gmail.com>
2017-08-10 15:42:52 -0400

    tested call to jobs api, working, added boostrap cdn

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import map from 'lodash/map'
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      value: '',
+      data: null
+    }
+  }
+
+  componentDidMount(){
+    axios.get('https://jason-jobs-bacon.herokuapp.com/api/v1/jobs').then(response => {
+      console.log(response.data.data);
+      this.setState({ data: response.data.data })
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>hello</h1>
+        <h1>{this.state.value}</h1>
+        {
+          this.state.data && map(this.state.data, (job, key) =>
+            <ul key={key} className="jobs-list">
+              <h2>Title:</h2><li>{job.title}</li>
+              <h2>Field:</h2><li>{job.field}</li>
+              <h2>Skill:</h2><li>{job.key_skill}</li>
+              <h2>Description:</h2><li>{job.description}</li>
+            </ul>
+          )
+        }
       </div>
     );
   }

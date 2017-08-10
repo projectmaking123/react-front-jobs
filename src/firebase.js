c28f3bbc541b19480d232e07b7daedc8cdda04c7
projectmaking123 <jason.89.wang@gmail.com>
2017-08-10 15:59:29 -0400

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyBRACpfRYAg-b0H7OD2kimG48q380kEw00",
+  authDomain: "jason-wang-1989.firebaseapp.com",
+  databaseURL: "https://jason-wang-1989.firebaseio.com",
+  projectId: "jason-wang-1989",
+  storageBucket: "",
+  messagingSenderId: "196549394865"
+ };
+firebase.initializeApp(config);
+
+export default firebase;
